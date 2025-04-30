451f00ec8f1fea7578ab20cb83c1c6087cbb6474
chach <chach@172.17.68.215>
2025-04-30 10:18:29 +0200

    mise à jour de fiabilité

---
+++ src/main/java/com/mycompany/projet2/Fiabilite.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projet2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author chach
+ */
+public class Fiabilite {
+    public static void EtudeFiabilite(ArrayList<Machine> LM){
+        int n = LM.size();
+        int f; // c'est la fiabilité
+        
+    }
+    
+    //faire une méthode de trie par ordre croissant voir premier semestre
+    
+    public static String[] TrieFiabilite(int n){
+        String[] F = new String[n];
+        
+        
+        return F; //renvoie le tableau rangé ds l'ordre croissant des machines par leur fiabilité
+    }
+        
+}
