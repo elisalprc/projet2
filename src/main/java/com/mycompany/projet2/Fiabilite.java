e7bfff0a8d69189390e23b720a2267a2edc76340
chach <chach@172.17.68.215>
2025-05-13 16:33:40 +0200

    mise à jour finale de la class Fiabilite pour calculer la fiabilité des machines de l'atelier et les trier par ordre croissant de fiabilité !!!

--- src/main/java/com/mycompany/projet2/Fiabilite.java
+++ src/main/java/com/mycompany/projet2/Fiabilite.java
@@ -12,6 +12,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
@@ -79,76 +82,102 @@ public class Fiabilite {
         return To;
     }
     
-    public static int Temps_Arret(ArrayList<Integer> Indice_retenu){ 
+    public static int Temps_Arret(String machine) {
     //pour les formats des dates : sera utile pour le calcul du temps d'arret
-        String formatDate = "dd/MM/yyyy";
-        DateTimeFormatter formatteurD = DateTimeFormatter.ofPattern(formatDate);
-        String formatHeure = "HH:mm";
-        DateTimeFormatter formatteurH = DateTimeFormatter.ofPattern(formatHeure);
+        String format = "dd/MM/yyyy-HH:mm";
+        DateTimeFormatter formatteur = DateTimeFormatter.ofPattern(format);
         
-    //on va récuperer les différents temps d'arrets relevés sur la periode d'observation (unité : minute) et les additionner ensemble  
-        int TpsArret = 0; //initialisation de la variable qui va compter la durée des temps d'arret (en minute)
-        for (int i =0; i<(Indice_retenu.size())-1; i=i+2){
-           String DateDepart = Liste_Date.get(i+1);
-           String DateArret = Liste_Date.get(i);
-           String HoraireDepart = Liste_Horaire.get(i+1);
-           String HoraireArret = Liste_Horaire.get(i);
-           LocalDateTime DD = LocalDateTime.parse(DateDepart, formatteurD);
-           LocalDateTime DA = LocalDateTime.parse(DateArret, formatteurD);
-           LocalDateTime HD = LocalDateTime.parse(DateDepart, formatteurH);
-           LocalDateTime HA = LocalDateTime.parse(DateArret, formatteurH);
-    //on va maintenant calculer la différence entre les dates (différence en unité de temps : minute)
-        long differenceInMinutesD = ChronoUnit.MINUTES.between(DA, DD);
-        long differenceInMinutesH = ChronoUnit.MINUTES.between(HA, HD);
-        TpsArret = TpsArret + (int) differenceInMinutesD + (int) differenceInMinutesH;
+    //on initialise le temps d'arret à 0 et la machine est considérée en marche (le fichier n'est pas encore parcouru donc on part du principe qu'aucune machine ne s'y trouve : cas idéal où tout fonctionne bien dans le meilleur des mondes)
+        int TpsArret = 0;
+        LocalDateTime arretEnCours = null;
+        boolean machinePresente = false;
+
+    //on va calculer le temps d'arret d'une machine, on procède de la façon suivante : 
+           //on parcours Liste_Machine (donc les machines dans l'odrdre présentent dans le fichier texte)
+           //si la machine entrée s'y trouve, on la note présente (ce qui implique que l'on va calculer son temps d'arret)
+           //sinon, la machine reste introuvée -> temps d'arret nul.
+    for (int i = 0; i < Liste_Machine.size(); i++) {
+        if (machine.equals(Liste_Machine.get(i))) {
+            machinePresente = true;
+            String evenement = Liste_Evenement.get(i);
+            String dateHeure = Liste_Date.get(i) + "-" + Liste_Horaire.get(i);
+            LocalDateTime dateEvt = LocalDateTime.parse(dateHeure, formatteur);
+
+            if (evenement.equals("A")) {
+                // Si on trouve un nouvel arrêt alors qu'il y en avait déjà un en cours (donc pas fini) : on considère que le précédent arrêt va jusqu'à ce nouvel arrêt. (pour éviter les erreurs inutiles)
+                if (arretEnCours != null) {
+                    long diff = ChronoUnit.MINUTES.between(arretEnCours, dateEvt);
+                    if (diff > 0){
+                        TpsArret = TpsArret + (int)diff;
+                    }
+                }
+                arretEnCours = dateEvt;
+            } else if (evenement.equals("D") && arretEnCours != null) {
+                long diff = ChronoUnit.MINUTES.between(arretEnCours, dateEvt);
+                if (diff > 0){
+                    TpsArret = TpsArret + (int)diff;
+                }
+                arretEnCours = null;
+            }
         }
-        System.out.println("la durée des tps d'arrets observée en minute est : "+TpsArret);
-    //retour de la durée des temps d'arret de la machine observée (donc aux indices de Indice_retenu)
-        return TpsArret; 
     }
+
+    // Si la machine était présente et qu'il reste un arrêt, on va prendre pour durée d'arrêt la période qui va de l'arrêt à aujourd'hui
+    if (machinePresente && arretEnCours != null) {
+        LocalDateTime Aujourdhui = LocalDateTime.now();
+        long diff = ChronoUnit.MINUTES.between(arretEnCours, Aujourdhui);
+        if (diff > 0){
+            TpsArret += diff;
+        }
+    }
+
+    // D'où si la machine n'apparait jamais dans le fichier : temps d'arrêt = 0
+    return TpsArret;
+}
     
     
 //pour calculer la fiabilité d'une machine donnée
-    public static int CalculFiabilite (String machine){
-        int i, f; // f c'est la fiabilité
+    public static double CalculFiabilite (String machine){ 
+        //on récupère les données du fichier
+        Recup_Donnees_Fichier_Texte();
         
         //creation d'une arrayliste des indices trouvés
         ArrayList<Integer> Indice_retenu = new ArrayList<>();
         Indice_retenu.clear(); //on va la réinitialiser à chaque fois pour éviter les erreurs
-        
-        //on récupère les données du fichier
-        Recup_Donnees_Fichier_Texte();
-        
+       
         //on va parcourir la liste des Machines à la recherche de la machine dont on souhaite déterminer la fiabilité
-        for (i=0; i< (Liste_Machine.size()-1); i++){
-            //Indice_retenu.clear(); //on va la réinitialiser à chaque boucle pour éviter les erreurs
-            if (machine == Liste_Machine.get(i)){
+        for (int i=0; i< Liste_Machine.size(); i++){
+            if (machine.equals(Liste_Machine.get(i))){ //on utilise .equals() pour comparer des chaïnes de caractère et pas == !!!!!
                 Indice_retenu.add(i);
             }
         }
         
         //pour vérifier on les affiche ds la console
-        for (i=0; i< (Indice_retenu.size()-1); i++){
+        for (int i=0; i< (Indice_retenu.size()-1); i++){
             System.out.println("Indice_retenu.get("+i+"): "+Indice_retenu.get(i));
         }
         
-        //fiabilité c  1 - (temps d'arrêt : Temps_Arret() /temps d'observation : Temps_Observation())
-        f = (1-(Temps_Arret(Indice_retenu)/Temps_Observation()));
-        return 0;
+        //initialisation des différents temps necessaire au calcul de la fiabilité
+        int tempsArret = Temps_Arret(machine);
+        int tempsObservation = Temps_Observation();
+        
+        //fiabilité : 1 - (temps d'arrêt/temps d'observation)
+        double fiabilite = (1 - ((double) tempsArret / (double) tempsObservation)); //double pour résultat décimal sinon on aura 0 ou 1
+        return fiabilite;
     }
     
     
 //pour étudier la fiabilité de toutes les machines de l'atelier
-    public static ArrayList<Integer> EtudeFiabilite(ArrayList<String> LMA){
-        ArrayList<Integer> Liste_Fiabilite_des_Machines = new ArrayList<>();
+    public static ArrayList<Double> EtudeFiabilite(ArrayList<String> LMA){
+        ArrayList<Double> Liste_Fiabilite_des_Machines = new ArrayList<>();
         
     //on va parcourir la liste des machines de l'atelier (LMA) et calculer pour chaque machine la fiabilité.    
         int i;
-        for (i=0; i<(LMA.size()-1); i++){
+        for (i=0; i<(LMA.size()); i++){
             String machine_de_atelier = LMA.get(i);
-            int f = CalculFiabilite (machine_de_atelier);
-            System.out.println(f);
-            Liste_Fiabilite_des_Machines.add(f); //donc pour la machine 3 (indice 4 de LMA), la fiabilité sera rangé à l'indice 4 de Liste_Fiabilite_des_Machines.
+            double fiabilite = CalculFiabilite (machine_de_atelier);
+            System.out.println(fiabilite);
+            Liste_Fiabilite_des_Machines.add(fiabilite); //donc pour la machine 3 (indice 4 de LMA), la fiabilité sera rangé à l'indice 4 de Liste_Fiabilite_des_Machines.
         }
         
         return Liste_Fiabilite_des_Machines;
@@ -156,12 +185,24 @@ public class Fiabilite {
     
     //faire une méthode de trie par ordre croissant voir premier semestre
     
-    public static String[] TrieFiabilite(int n){
-        String[] F = new String[n];
-        
-        
-        return F; //renvoie le tableau rangé ds l'ordre croissant des machines par leur fiabilité
+    public static String[] TrieFiabilite(ArrayList<Double> Liste_Fiabilite_des_Machines, ArrayList<String> LMA) {
+    //on crée une map pour associer les machines à leur fiabilité : c'est plus facile de traiter une liste à trier plutôt que d'en trier deux en parallèle, ça devrait limiter la casse...
+        Map<String, Double> map = new HashMap<>();
+    for (int i = 0; i < LMA.size(); i++) {
+        map.put(LMA.get(i), Liste_Fiabilite_des_Machines.get(i));
     }
+
+    // On trie ici les entrées de la map en fonction de la fiabilité
+    List<Map.Entry<String, Double>> list = new ArrayList<>(map.entrySet());
+    list.sort(Map.Entry.comparingByValue()); //sort() est une méthode pour trier des éléments par ordre croissant !!
+
+    // On crée un tableau de résultat que l'on va renvoyer
+    String[] F = new String[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+        F[i] = list.get(i).getKey() + " (fiabilité: " + list.get(i).getValue() + ")";
+    }
+    return F;
+}
         
 }
 
@@ -198,3 +239,21 @@ public class Fiabilite {
 //import static com.mycompany.projet2.Fiabilite.EtudeFiabilite;
 //import static com.mycompany.projet2.Fiabilite.Temps_Observation;
 //import java.util.ArrayList;
+
+//ArrayList<Machine> LM = new ArrayList<>();
+  //      initialisationLM(LM);
+    //    ArrayList<String> LMA = new ArrayList<>();
+      //  for (int i=0; i< (LM.size()); i++){
+        //   LMA.add("Mach_"+(i+1));
+          // System.out.println(LMA.get(i));
+        //}
+        //CalculFiabilite(LMA.get(1));
+        //EtudeFiabilite(LMA);
+        
+        //System.out.println(EtudeFiabilite(LMA));
+        //Temps_Observation();
+        //System.out.println(Temps_Observation());
+        //String[] F= TrieFiabilite(EtudeFiabilite(LMA),LMA );
+        //for (int i=0; i<F.length; i++ ){
+         // System.out.println(F[i]); 
+        //}
